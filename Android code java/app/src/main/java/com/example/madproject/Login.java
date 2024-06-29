813aaeb2ad3bb323ce850c295429328ff641b74b
Prabhath Madushanka <143389126+PrabhathShanka@users.noreply.github.com>
2024-06-30 00:37:17 +0530

    update

--- Android code java/app/src/main/java/com/example/madproject/Login.java
+++ Android code java/app/src/main/java/com/example/madproject/Login.java
@@ -1,6 +1,10 @@
 package com.example.madproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,17 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView button = findViewById(R.id.createText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+
+        });
+
     }
 }
