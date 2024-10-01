c7c4fc9b977a3c62c7bb8d156d29eb999c60d5ac
Prabhath Madushanka <143389126+PrabhathShanka@users.noreply.github.com>
2024-10-01 13:23:47 +0530

    update

--- Android code java/app/src/main/java/com/example/madproject/settings.java
+++ Android code java/app/src/main/java/com/example/madproject/settings.java
@@ -1,12 +1,16 @@
 package com.example.madproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class settings extends AppCompatActivity {
 
@@ -20,5 +24,11 @@ public class settings extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Back(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Dashboard1.class));
+        finish();
     }
 }
